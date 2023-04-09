246856dcfdcc4f86ede2268eb3349a58317579ee
Kirill <shkolenko02-03-2004@mail.ru>
2023-04-09 09:52:16 +0300

    Make predeployment magic ^)

--- FileCVS_app/app.py
+++ FileCVS_app/app.py
@@ -1,15 +1,47 @@
-from fastapi import FastAPI, UploadFile, File, Form
+from fastapi import FastAPI, UploadFile, \
+    File, Form, APIRouter, Request
 from fastapi.middleware.cors import CORSMiddleware
 import os
 from utils import Git
 from model.model import DetailFileData
 from dotenv import load_dotenv
+from fastapi.openapi.docs import (
+    get_swagger_ui_html,
+)
 
-app = FastAPI(docs_url="/", redoc_url=None)
+api = APIRouter()
 
 load_dotenv()
 
 git = Git()
+
+
+
+@api.get("/commits/{file_name}")
+def get_commits(file_name: str):
+    return git.get_commits(file_name)
+
+
+@api.get("/files")
+def get_files():
+    return git.get_files()
+
+
+@api.post("/commit")
+def get_commit(file: DetailFileData):
+    return git.get_commit(file.commit_name, file.file_name)
+
+
+@api.post("/new_commit")
+async def new_commit(file_name: str = Form(...),
+                     commit_name: str = Form(...),
+                     file: UploadFile = File(...)):
+    file = await file.read()
+    return git.new_commit(file_name, commit_name, file)
+
+app = FastAPI(redoc_url=None,
+              openapi_url="/api/cvs/openapi.json",
+              docs_url="/api/cvs/docs")
 
 app.add_middleware(
     CORSMiddleware,
@@ -19,25 +51,4 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-@app.get("/commits/{file_name}")
-def get_commits(file_name: str):
-    return git.get_commits(file_name)
-
-
-@app.get("/files")
-def get_files():
-    return git.get_files()
-
-
-@app.post("/commit")
-def get_commit(file: DetailFileData):
-    return git.get_commit(file.commit_name, file.file_name)
-
-
-@app.post("/new_commit")
-async def new_commit(file_name: str = Form(...),
-                     commit_name: str = Form(...),
-                     file: UploadFile = File(...)):
-    file = await file.read()
-    return git.new_commit(file_name, commit_name, file)
+app.include_router(api, prefix='/api/cvs', tags=['cvs'])
--- calendar_app/app.py
+++ calendar_app/app.py
@@ -1,6 +1,6 @@
 import json
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request, APIRouter
 from fastapi.middleware.cors import CORSMiddleware
 from utils import Calendar
 from models.models import Event
@@ -9,7 +9,9 @@ from dotenv import load_dotenv
 from googleapiclient.errors import HttpError
 
 
-app = FastAPI(docs_url="/", redoc_url=None)
+api = APIRouter()
+
+
 
 load_dotenv()
 
@@ -19,32 +21,26 @@ calendar_id = os.getenv("CALENDAR_ID")
 calendar = Calendar(file_path, scopes, calendar_id)
 
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins='*',
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 
-@app.get("/calendars")
+
+@api.get("/all")
 def get_calendar_list():
     return calendar.get_calendar_list()
 
 
-@app.get("/events")
+@api.get("/events")
 def get_events():
     return calendar.get_events_list()
 
 
-@app.post("/events")
+@api.post("/events")
 def create_events(event: Event):
     print(Event)
     return calendar.add_event(event)
 
 
-@app.delete("/events/{eventId}")
+@api.delete("/events/{eventId}")
 def create_events(eventId: str):
     try:
         response = calendar.delete_event(eventId)
@@ -53,12 +49,26 @@ def create_events(eventId: str):
     return response
 
 
-@app.patch("/events/{eventId}")
+@api.patch("/events/{eventId}")
 def create_events(body: Event, eventId: str):
     body = json.loads(body.json(exclude_unset=True))
     try:
         response = calendar.patch_event(eventId, body)
     except HttpError as e:
         raise HTTPException(status_code=404, detail=str(e))
-    print(response)
     return response
+
+
+app = FastAPI(redoc_url=None,
+              openapi_url="/api/calendar/openapi.json",
+              docs_url="/api/calendar/docs")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins='*',
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(api, prefix='/api/calendar', tags=['calendar'])
